85984b667933ec9ea8d96a0101d9e2597a218cf7
Kevin Earls <kearls@redhat.com>
2022-01-18 14:08:17 +0100

    Initial Commit

---
+++ MyEmojiArt/MyEmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyEmojiArtApp.swift
+//  MyEmojiArt
+//
+//  Created by Kevin Earls on 18/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyEmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
